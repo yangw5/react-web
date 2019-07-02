993f82d6a7e5f9d42185d6bfe90a045baddf1a73
杨文伍 <yangw5@163.com>
2019-07-02 13:17:36 +0800

    'install'

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
