d9205b39cc2a9bd662b2efc12a65c182cac9fd87
杨文伍 <yangw5@163.com>
2019-07-09 12:42:33 +0800

    实现：form表单的自动填充 验证 getFieldDecorator

--- src/component/widget/FromInput.js
+++ src/component/widget/FromInput.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Form, Input } from "antd";
 
-export default props => {
+export default ({ initialValue, label, max, form, placeholder, field }) => {
+  let { getFieldDecorator } = form;
+  let rules = [
+    {
+      message: `请输入${label}`,
+      transform: value => {
+        if (value && value.trim) value = value.trim();
+        // if ((value && !window._.isString(value)) || Number.isInteger(value))
+        //   return JSON.stringify(value);
+        return value;
+      }
+    }
+  ];
+  if (max) rules.push({ max, message: `不能输入超过${max}个字符` });
   return (
-    <Form.Item {...props}>
-      <Input placeholder="input with clear icon" allowClear />
+    <Form.Item>
+      {getFieldDecorator(field, {
+        initialValue,
+        rules
+      })(<Input placeholder={placeholder || `请输入${label}`} />)}
     </Form.Item>
   );
 };
--- src/component/widget/FromSearch.js
+++ src/component/widget/FromSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Form } from "antd";
+import { Form, Button } from "antd";
+import CFromInput from "./CFromInput";
 import FromInput from "./FromInput";
 
 class FromSearch extends Component {
@@ -9,12 +10,26 @@ class FromSearch extends Component {
       data: ""
     };
   }
+  onsumit = () => {
+    let data = this.props.form.getFieldsValue();
+    alert(data["password"]);
+    alert(data["userName"]);
+  };
   render() {
+    let { form } = this.props;
     return (
       <div>
-        <Form>
-          <FromInput />
-        </Form>
+        <CFromInput
+          label={"姓名"}
+          field={"userName"}
+          max={50}
+          required={true}
+        />
+        <p>----------------------</p>
+        <FromInput max={5} form={form} field="password" label={"密码"} />
+        <Button onClick={this.onsumit} type="primary">
+          FromSearch提交
+        </Button>
       </div>
     );
   }
